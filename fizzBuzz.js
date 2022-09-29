43160f4ec50ef68010221515db8f5d565e1a4b54
Cameron Miner <cameronsminer@gmail.com>
2022-09-29 10:07:01 -0600

    Added code challenge instructions as comment

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,3 +1,7 @@
+// Write a program that counts from 1 to 50 in fizzbuzz fashion.
+
+// To do so, loop from 1 to 50 (inclusive). Each time through, if the number is evenly divisible by 3, say ‘fizz’. If the number is evenly divisible by 5, say ‘buzz’. If the number is evenly divisible by both 3 and 5, say ‘fizzbuzz’. Otherwise, say the number.
+
 // Write your solution below this line:
 
 for (let i = 1; i < 51; i++) {
